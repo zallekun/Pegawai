ac27e175a4bf590a9168d899224d5b0f62dc9c64
Rezal Suryadi Putra <suryadirezal@gmail.com>
2025-05-24 09:23:23 +0700

    tambah fitur profile linked in di page tentang

--- app/Views/tentang/tentang.php
+++ app/Views/tentang/tentang.php
@@ -1,18 +1,48 @@
-
 <?= $this->extend('layout/main'); ?>
 
 <?= $this->section('content'); ?>
 <h2>Tentang Kami</h2>
-<p>Aplikasi ini dibuat oleh Kelompok kami:
-    <br>
-    -2250081098 Muhammad Arivaldi
-    <br>
-    -2250081099 Ahmad Haidar
-    <br>
-    -2250081103 Bagja Iskandar Jamil
-    <br>
-    -2250081107 Rezal Suryadi Putra
-
-
-</p>
+<p>Aplikasi ini dibuat oleh mahasiswa semester 6:</p>
+<div class="row">
+    <div class="col-md-4 mb-3">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <img src="https://ui-avatars.com/api/?name=Muhammad+Arivaldi" class="rounded-circle mb-2" width="80" height="80" alt="Muhammad Arivaldi">
+                <h5 class="card-title mb-1">Muhammad Arivaldi</h5>
+                <p class="card-text">2250081098</p>
+                <a href="https://www.linkedin.com/in/username-arivaldi" target="_blank" class="btn btn-outline-primary btn-sm">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 mb-3">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <img src="https://ui-avatars.com/api/?name=Ahmad+Haidar" class="rounded-circle mb-2" width="80" height="80" alt="Ahmad Haidar">
+                <h5 class="card-title mb-1">Ahmad Haidar</h5>
+                <p class="card-text">2250081099</p>
+                <a href="https://www.linkedin.com/in/username-haidar" target="_blank" class="btn btn-outline-primary btn-sm">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 mb-3">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <img src="https://ui-avatars.com/api/?name=Bagja+Iskandar+Jamil" class="rounded-circle mb-2" width="80" height="80" alt="Bagja Iskandar Jamil">
+                <h5 class="card-title mb-1">Bagja Iskandar Jamil</h5>
+                <p class="card-text">2250081103</p>
+                <a href="https://www.linkedin.com/in/username-bagja" target="_blank" class="btn btn-outline-primary btn-sm">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 mb-3">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <img src="https://ui-avatars.com/api/?name=Rezal+Suryadi+Putra" class="rounded-circle mb-2" width="80" height="80" alt="Rezal Suryadi Putra">
+                <h5 class="card-title mb-1">Rezal Suryadi Putra</h5>
+                <p class="card-text">2250081107</p>
+                <a href="https://www.linkedin.com/in/rezalsuryadiputra" target="_blank" class="btn btn-outline-primary btn-sm">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+</div>
 <?= $this->endsection(); ?>
