303a10a31c7d3e146ae96f90e83436c42ae22250
Rezal Suryadi Putra <166518465+zallekun@users.noreply.github.com>
2025-06-24 19:56:08 +0700

    Update tentang.php

--- app/Views/tentang/tentang.php
+++ app/Views/tentang/tentang.php
@@ -14,5 +14,35 @@
             </div>
         </div>
     </div>
+     <div class="col-md-4 mb-3">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <img src="https://ui-avatars.com/api/?name=Muhammad+Arivaldi" class="rounded-circle mb-2" width="80" height="80" alt="Rezal Suryadi Putra">
+                <h5 class="card-title mb-1">Muhammad Arivaldi</h5>
+                <p class="card-text">2250081098</p>
+                <a href="https://www.linkedin.com/in/rezalsuryadiputra" target="_blank" class="btn btn-outline-primary btn-sm">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 mb-3">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <img src="https://ui-avatars.com/api/?name=Ahmad+Haidar" class="rounded-circle mb-2" width="80" height="80" alt="Rezal Suryadi Putra">
+                <h5 class="card-title mb-1">Ahmad Haidar</h5>
+                <p class="card-text">2250081099</p>
+                <a href="https://www.linkedin.com/in/rezalsuryadiputra" target="_blank" class="btn btn-outline-primary btn-sm">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 mb-3">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <img src="https://ui-avatars.com/api/?name=Bagja+Iskandar+Jamil" class="rounded-circle mb-2" width="80" height="80" alt="Rezal Suryadi Putra">
+                <h5 class="card-title mb-1">Bagja Iskandar Jamil</h5>
+                <p class="card-text">2250081103</p>
+                <a href="https://www.linkedin.com/in/rezalsuryadiputra" target="_blank" class="btn btn-outline-primary btn-sm">LinkedIn</a>
+            </div>
+        </div>
+    </div>
 </div>
 <?= $this->endsection(); ?>
